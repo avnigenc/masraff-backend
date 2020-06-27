81859233b6bcfdad07a1dc3dfc7456bdb3d4be28
Avni Genç <avnignc@gmail.com>
2020-06-27 18:06:02 +0300

    migrations

---
+++ src/migrations/1593260810763-new-models.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class newModels1593260810763 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
